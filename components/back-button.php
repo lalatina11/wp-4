c3ef31d31243e4bd4d7ac59093a9e302d1e5ec30
lalatina11 <rahmadancandra@gmail.com>
2025-05-08 21:11:35 +0700

    build: clean up main branch, keep only README.md for docs

--- components/back-button.php
+++
@@ -1,8 +0,0 @@
-<button class="btn btn-primary fw-semibold" id="backBTN">Back</button>
-
-
-<script>
-    document.getElementById("backBTN").addEventListener("click", () => {
-        history.back()
-    })
-</script>
