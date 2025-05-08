6d86446eca8c8a8b8af0c9644e9f33e82ad93672
lalatina11 <rahmadancandra@gmail.com>
2025-05-08 09:17:30 +0700

    branch wp-4

--- components/back-button.php
+++ components/back-button.php
@@ -1,6 +1,5 @@
 <button class="btn btn-primary fw-semibold" id="backBTN">Back</button>
 
-
 <script>
     document.getElementById("backBTN").addEventListener("click", () => {
         history.back()
