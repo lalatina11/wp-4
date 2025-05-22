465b7a3647407ae9535cf67489eb273295ef5800
lalatina11 <rahmadancandra@gmail.com>
2025-05-22 08:22:29 +0700

    refactor: replace css with table

--- index.php
+++ index.php
@@ -14,20 +14,37 @@
         </h3>
         <form
             style="display: flex;flex-direction: column;max-width: 450px;margin: auto; justify-content: center;align-items: center;gap: 16px;"
-            action="">
-            <div style="display: flex;flex-direction: column;gap: 8px;">
-                <label for="nama">Nama</label>
-                <input type="text" name="nama" id="nama" />
-            </div>
-            <div style="display: flex;flex-direction: column;gap: 8px;">
-                <label for="tel">No Telepon</label>
-                <input type="tel" name="tel" id="tel" />
-            </div>
-            <div style="display: flex;flex-direction: column;gap: 8px;">
-                <label for="alamat">Alamat</label>
-                <textarea name="alamat" id="alamat"></textarea>
-            </div>
-            <div style="display: flex; gap: 8px;">
+            action="table.php">
+            <table border="1">
+                <tr>
+                    <td>
+                        <label for="nama">Nama</label>
+                    </td>
+                    <td>:</td>
+                    <td>
+                        <input type="text" name="nama" id="nama" />
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <label for="tel">No Telepon</label>
+                    </td>
+                    <td>:</td>
+                    <td>
+                        <input type="tel" name="tel" id="tel" />
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <label for="alamat">Alamat</label>
+                    </td>
+                    <td>:</td>
+                    <td>
+                        <textarea name="alamat" id="alamat"></textarea>
+                    </td>
+                </tr>
+            </table>
+            <div>
                 <input style="flex: 1/2;" class="btn btn-secondary btn-wide" type="reset" name="" id="">
                 <button style="flex: 1/2;" class="btn btn-secondary btn-wide">Submit</button>
             </div>
