1aa3dfc29a0b2c35426373f43928b688804817e5
lalatina11 <rahmadancandra@gmail.com>
2025-06-04 20:00:41 +0700

    feat: add dark mode at back button

--- components/back-button.php
+++ components/back-button.php
@@ -1,4 +1,7 @@
-<button class="btn btn-primary fw-semibold" id="backBTN">Back</button>
+<div style="display: flex; gap: 12px; align-items: center;">
+    <button class="btn btn-primary fw-semibold" id="backBTN">Back</button>
+    <?php include "dark-mode.php" ?>
+</div>
 
 
 <script>
