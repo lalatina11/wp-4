a6a9e1c58ad95f889428b17ffbb1ab44d7538cc3
lalatina11 <rahmadancandra@gmail.com>
2025-06-04 19:12:27 +0700

    refactor: replace option with reusability components

---
+++ components/option.php
@@ -0,0 +1,12 @@
+                    <tr>
+                        <td><label for="pilih">Pilih</label></td>
+                        <td>:</td>
+                        <td>
+                            <input type="radio" name="pilih" id="pilih" value="luas">
+                            <label for="">Luas</label>
+                            <input type="radio" name="pilih" id="pilih" value="keliling">
+                            <label for="">Keliling</label>
+                            <input type="radio" name="pilih" id="pilih" value="semua">
+                            <label for="">Semua</label>
+                        </td>
+                    </tr>
