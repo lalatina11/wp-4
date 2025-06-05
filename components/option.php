78b1c64474a8fed6ae05878c4d5ad650e912c6a6
lalatina11 <rahmadancandra@gmail.com>
2025-06-05 08:07:33 +0700

    style: update option ui

--- components/option.php
+++ components/option.php
@@ -1,12 +1,20 @@
-                    <tr>
-                        <td><label for="pilih">Pilih</label></td>
-                        <td>:</td>
-                        <td>
-                            <input type="radio" name="pilih" id="pilih" value="luas">
-                            <label for="">Luas</label>
-                            <input type="radio" name="pilih" id="pilih" value="keliling">
-                            <label for="">Keliling</label>
-                            <input type="radio" name="pilih" id="pilih" value="semua">
-                            <label for="">Semua</label>
-                        </td>
-                    </tr>
+<tr>
+    <td><label for="pilih">Pilih</label></td>
+    <td>:</td>
+    <td>
+        <div style="display: flex;gap: 12px;align-items: center;justify-content: center;">
+            <div style="display: flex;gap: 4px;align-items: center;">
+                <input type="radio" name="pilih" id="luas" value="luas">
+                <label for="luas">Luas</label>
+            </div>
+            <div style="display: flex;gap: 4px;align-items: center;">
+                <input type="radio" name="pilih" id="keliling" value="keliling">
+                <label for="keliling">Keliling</label>
+            </div>
+            <div style="display: flex;gap: 4px;align-items: center;">
+                <input type="radio" name="pilih" id="semua" value="semua">
+                <label for="semua">Semua</label>
+            </div>
+        </div>
+    </td>
+</tr>
